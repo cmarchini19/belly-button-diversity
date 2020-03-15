7959190e89d85a1f3a25163d7cf3cb64cb2d6b72
Chris Marchini <cmarchini@rti.org>
2020-03-14 22:59:20 -0400

    Final app.js - main activities + bonus completed

--- Starter_Code/static/js/app.js
+++ Starter_Code/static/js/app.js
@@ -29,7 +29,38 @@ function buildcharts(sampleID) {
         var sample_values = result.sample_values;
         var otu_ids = result.otu_ids;
         var otu_labels = result.otu_labels;
-       
+
+        
+        // OPTIONAL - data for gauge chart
+        var gaugeresults = data.metadata
+        var gaugearray = gaugeresults.filter(gaugesample => gaugesample.id == sampleID);
+        var gaugeresult = gaugearray[0]
+            console.log(gaugeresult)
+        var gaugecurrentid = gaugeresult.id;
+
+        // OPTIONAL - Establish gauge chart var
+        var gaugemeta = gaugeresults.filter(s => {return +s.id === +gaugecurrentid}); 
+        var gaugechart = {
+            type: "indicator",
+            mode: "gauge+number",
+            value: gaugemeta.map(value => +value.wfreq)[0],
+            title: { text: "Scrubs per Week"},
+            gauge: {
+                axis: { range: [0, 9]},
+                steps: [
+                    { range: [0, 1], color: "#d6dcd0" },
+                    { range: [1, 2], color: "#c6d8b6" },
+                    { range: [2, 3], color: "#b7d49c" },
+                    { range: [3, 4], color: "#a8d182" },
+                    { range: [4, 5], color: "#99cd68" },
+                    { range: [5, 6], color: "#8ac94e" },
+                    { range: [6, 7], color: "#7bc634" },
+                    { range: [7, 8], color: "#6cc21a" },
+                    { range: [8, 9], color: "#5dbf00" }
+                  ]
+            }
+        }
+        
         // Establish bar chart var
         var barchart = {
             x: sample_values.slice(0, 10).reverse(),
@@ -51,7 +82,7 @@ function buildcharts(sampleID) {
             }
         };
         
-         // Set-up bar chart data and layout var for PlotLy
+        // Set-up bar chart data and layout var for PlotLy
         var bardata = [barchart]
         var barlayout = {
             title: "Top 10 OTUs for Test Subject",
@@ -70,9 +101,17 @@ function buildcharts(sampleID) {
             width: 1200
         }
     
+        // Set-up gauge chart data and layout var for PlotLy
+        var gaugedata = [gaugechart]
+        var gaugelayout = {
+            title: "Belly Button Washing Frequency",
+            margin: {t: 30, l: 75}
+        }
+
         // Plotly Charts
         Plotly.newPlot("bar", bardata, barlayout);
         Plotly.newPlot("bubble", bubbledata, bubblelayout)
+        Plotly.newPlot("gauge", gaugedata, gaugelayout)
     });
 }
 
@@ -115,6 +154,7 @@ function buildmetadata(demodata) {
             .text(`wfreq: ${currentmeta.map(meta => meta.wfreq)}`)
     });
 }
+
 
 //Test Subject Dashboard Updates
 function optionChanged(newSample) {
